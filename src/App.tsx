ff81f99d692fd965de26cabd84e83f55ee9a103c
Luan <luanmedicina08@gmail.com>
2026-01-13 00:28:10 -0300

    React Antigravity Starter inicial

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ padding: 24 }}>
+      <h1>Meu React Starter</h1>
+      <p>Base para criar novos apps (Antigravity, Electron, etc.).</p>
+    </div>
+  );
+}
+
+export default App;
